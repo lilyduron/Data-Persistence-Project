a57687f02530face34c11bdc52c4061804b7029f
lilyduron <47832265+lilyduron@users.noreply.github.com>
2025-02-11 14:06:02 +0100

    Exit functionality
    Exit the app button and back to menu fuctionalities added.

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MenuUIHandler : MonoBehaviour
 {
@@ -12,6 +15,21 @@ public class MenuUIHandler : MonoBehaviour
      public void StartNew()
     {
        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        //this is when we use the app in the Unity editor, so the exit button works
+        #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+        #else
+        Application.Quit(); // original code to quit Unity player
+        #endif
+    }
+
+    public void BackMenu()
+    {
+       SceneManager.LoadScene(0);
     }
 
     // Update is called once per frame
